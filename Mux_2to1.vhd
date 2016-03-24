38a7a7b075aec7af3b42db44f11296ebf4433861
AidanPetit <aidan.petit@mail.mcgill.ca>
2016-03-24 12:35:04 -0400

    Added 3to1 and 2to1 muxes

---
+++ Mux_2to1.vhd
@@ -0,0 +1,33 @@
+-- ECSE 425 - Comp Organization & Architecture - Final Project
+-- Group 5: Andrei Chubarau, Luis Gallet Zambrano, Aidan Petit, Wei Wang
+-- Author: Aidan Petit
+--
+-- Mux_2to1.vhd
+-- Generic 2 to 1 multipler. Select between in1 or in2 becased on sel
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+--Entity declaration
+Entity Mux_2to1 is
+	Port 
+	(	
+		--select line
+		sel		: in std_logic;
+		--data inputs
+		in1		: in std_logic_vector(31 downto 0);
+		in2		: in std_logic_vector(31 downto 0);
+		--output
+          	dataOout	: out std_logic_vector(31 downto 0)
+	);
+End Mux_2to1;
+
+--Architecture 
+Architecture mux of Mux_2to1 is
+    begin
+	case sel is
+  		when "0" =>	dataOut	<= in1;
+  		when "1" =>	dataOut <= in2;
+	end case;
+    end;
+END mux;
---
+++ Mux_3to1.vhd
@@ -0,0 +1,42 @@
+-- ECSE 425 - Comp Organization & Architecture - Final Project
+-- Group 5: Andrei Chubarau, Luis Gallet Zambrano, Aidan Petit, Wei Wang
+-- Author: Aidan Petit
+--
+-- Mux_3to1.vhd
+-- Generic 3 to 1 multipler. Select between in1 or in2 becased on sel. If 
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+--Entity declaration
+Entity Mux_2to1 is
+	Port 
+	(	
+		--select line
+		sel		: in std_logic_vector(1 downto 0);
+		--data inputs
+		in1		: in std_logic_vector(31 downto 0);
+		in2		: in std_logic_vector(31 downto 0);
+		in3		: in std_logic_vector(31 downto 0);
+		--output
+          	dataOout	: out std_logic_vector(31 downto 0)
+	);
+End Mux_2to1;
+
+--Architecture 
+Architecture mux of Mux_2to1 is
+
+signal highZ	: std_logic_vector(31 downto 0);
+
+BEGIN
+
+	highZ	<= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";	--indicates sel was set to '11' which shouldn't happen
+
+	case sel is
+  		when "00" =>	dataOut	<= in1;
+		when "01" =>	dataOut <= in2;
+		when "10" =>	dataOut <= in3;
+  		when "11" =>	dataOut <= highZ;
+	end case;
+    end;
+END mux;
