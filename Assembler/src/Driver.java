19b358a2ff51250ec3b1fe76fe3690b6ed4f9349
ch-andrei <im.andrei@hotmail.ca>
2016-03-24 21:49:52 -0400

    Fix to Driver.java

--- Assembler/src/Driver.java
+++ Assembler/src/Driver.java
@@ -5,17 +5,14 @@ import assembler_main.binary_instructions.toolset.Tools;
  * Created by Andrei-ch on 2016-03-19.
  */
 public class Driver {
-    // final variables
-    static final String filename_in = "addi.asm";
-    static final String path_in = "./resources/" + filename_in;
 
     /**
      * run assembler
      * @param args
      */
     public static void main(String[] args) {
-//        String read_from = args[0];
-        String write_to = path_in.replace(".asm", ".dat");
-        Assembler.assemble(path_in, write_to);
+        String read_from = args[0];
+        String write_to = read_from.replace(".asm", ".dat");
+        Assembler.assemble(read_from, write_to);
     }
 }
