832ceebe89f37eead3348f30e8a07361670f5a73
Wei <wei.wang18@mail.mcgill.ca>
2016-03-27 18:02:46 -0400

    modify the forwarding mux

--- CPUS/cpu/src/cpu.vhd
+++ CPUS/cpu/src/cpu.vhd
@@ -613,8 +613,8 @@ ALU_data0_Forward_Mux : Mux_3to1
   PORT MAP(
     sel      => ,--Forward Unit: in std_logic_vector(1 downto 0);
     in1      => ID_EX_data0_out,
-    in2      => Result_W,
-    in3      => DataMem_data,
+    in2      => EX_MEM_data,
+    in3      => Result_W,
     dataOout => ALU_data0
     );
 
@@ -622,8 +622,8 @@ ALU_data1_Forward_Mux : Mux_3to1
   PORT MAP(
     sel      => ,--Forward Unit
     in1      => ID_EX_data1_out,
-    in2      => Result_W,
-    in3      => DataMem_data,
+    in2      => EX_MEM_data,
+    in3      => Result_W,
     dataOout => t_ALU_data1
   );
 
