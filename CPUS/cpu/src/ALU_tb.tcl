5ec480b3ddf10f4533714cc91bf044b2fa63a399
Aidan Petit <aidan.petit@mail.mcgill.ca>
2016-03-27 20:23:44 -0400

    Added ALU_tb.tcl

---
+++ CPUS/cpu/src/ALU_tb.tcl
@@ -0,0 +1,36 @@
+puts {
+	ALU Test 
+	Author : Aidan Petit
+	Creation Date : 03/27/2016
+	Last Revision : 
+}
+
+proc AddWaves {} \
+{
+	# all relevant signals and appropriate radix's
+	add wave -position end sim:/ALU_tb/clk
+	add wave -radix binary -position end sim:/ALU_tb/t_opCode
+	add wave -radix binary -position end sim:/ALU_tb/t_data0
+	add wave -radix binary -position end sim:/ALU_tb/t_data1
+	add wave -radix binary -position end sim:/ALU_tb/t_data_out
+
+}
+
+vlib work
+
+# compile ALU and testbench
+
+vcom -reportprogress 300 -work work ALU.vhd
+vcom -reportprogress 300 -work work ALU_tb.vhd
+
+# start simulation
+vsim ALU_tb
+
+# Add the waves
+AddWaves
+
+# generate clock
+# force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1 ns
+
+# run for an appropriate amount of time
+run 8000ns
