45603ece9c0ae941d7ce545ec18af1f9409cc86b
AidanPetit <aidan.petit@mail.mcgill.ca>
2016-03-29 11:28:45 -0400

    Updated forwarding_tb script to match Forwarding_tb.vhd

--- CPUS/cpu/src/forwarding_tb.tcl
+++ CPUS/cpu/src/forwarding_tb.tcl
@@ -1,5 +1,5 @@
 puts {
-	Forward Unit Test 
+	Forward Unit Test Script
 	Author : Aidan Petit
 	Creation Date : 03/27/2016
 	Last Revision : 
@@ -8,14 +8,14 @@ puts {
 proc AddWaves {} \
 {
 	# all relevant signals and appropriate radix's
-	add wave -radix binary -position end sim:/forwarding_tb/t_EX_MEM_RegWrite
-	add wave -radix binary -position end sim:/forwarding_tb/t_MEM_WB_RegWrite 
-	add wave -radix binary -position end sim:/forwarding_tb/t_ID_EX_Rs
-	add wave -radix binary -position end sim:/forwarding_tb/t_ID_EX_Rt 
-	add wave -radix binary -position end sim:/forwarding_tb/t_EX_MEM_Rd 
-	add wave -radix binary -position end sim:/forwarding_tb/t_MEM_WB_Rd 
-	add wave -radix binary -position end sim:/forwarding_tb/t_F0_EX 
-	add wave -radix binary -position end sim:/forwarding_tb/t_F1_EX 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_EX_MEM_RegWrite
+	add wave -radix binary -position end sim:/Forwarding_tb/t_MEM_WB_RegWrite 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_ID_EX_Rs
+	add wave -radix binary -position end sim:/Forwarding_tb/t_ID_EX_Rt 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_EX_MEM_Rd 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_MEM_WB_Rd 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_F0_EX 
+	add wave -radix binary -position end sim:/Forwarding_tb/t_F1_EX 
 
 }
 
@@ -24,10 +24,10 @@ vlib work
 # compile ALU and testbench
 
 vcom -reportprogress 300 -work work Forwarding.vhd
-vcom -reportprogress 300 -work work forwarding_tb.vhd
+vcom -reportprogress 300 -work work Forwarding_tb.vhd
 
 # start simulation
-vsim forwarding_tb
+vsim Forwarding_tb
 
 # Add the waves
 AddWaves
