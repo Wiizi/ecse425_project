8c8a3b372114a7ee5a25416fd5a075f6322c92ec
ch-andrei <im.andrei@hotmail.ca>
2016-03-30 23:08:32 -0400

    Refactored

--- CPUS/cpu/scripts/Compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off		
-		
-pushd "%~dp0\..\quartus"		
-C:\altera\13.0sp1\modelsim_ase\win32aloem\vsim.exe -c -do "set topLevelDir \"[pwd]/..\"; source ../scripts/livecpulib.tcl; CompileCPU; quit"		
-popd		
-		
-		
-:EXIT		
