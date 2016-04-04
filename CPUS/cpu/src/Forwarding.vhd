71a258aa375901856ba7bb6100ee9b15eaf5e390
Wei <wei.wang18@mail.mcgill.ca>
2016-04-04 12:10:45 -0400

    forwarding

--- CPUS/cpu/src/Forwarding.vhd
+++ CPUS/cpu/src/Forwarding.vhd
@@ -13,13 +13,18 @@ USE ieee.numeric_std.all;
 
 entity Forwarding is
 	port(
+		Branch 			: in std_logic;
 		EX_MEM_RegWrite : in std_logic;
 		MEM_WB_RegWrite	: in std_logic;
-		EX_Rs		: in std_logic_vector(4 downto 0);
-		EX_Rt		: in std_logic_vector(4 downto 0);
-		MEM_Rd		: in std_logic_vector(4 downto 0);
-		WB_Rd		: in std_logic_vector(4 downto 0);
+		ID_Rs			: in std_logic_vector(4 downto 0);
+		ID_Rt			: in std_logic_vector(4 downto 0);
+		EX_Rs			: in std_logic_vector(4 downto 0);
+		EX_Rt			: in std_logic_vector(4 downto 0);
+		MEM_Rd			: in std_logic_vector(4 downto 0);
+		WB_Rd			: in std_logic_vector(4 downto 0);
 
+		Forward0_Branch	: out std_logic_vector(1 downto 0);
+		Forward1_Branch	: out std_logic_vector(1 downto 0);
 		Forward0_EX 	: out std_logic_vector(1 downto 0);
 		Forward1_EX		: out std_logic_vector(1 downto 0)
 		);
@@ -29,26 +34,60 @@ architecture Behavioural of Forwarding is
 
 begin
 
-	process(EX_MEM_RegWrite, MEM_WB_RegWrite, EX_Rs, EX_Rt, MEM_Rd, WB_Rd)
+	process(Branch, EX_MEM_RegWrite, MEM_WB_RegWrite, ID_Rs, ID_Rt, EX_Rs, EX_Rt, MEM_Rd, WB_Rd)
 	begin
 
-	Forward0_EX <= "00";
-	Forward1_EX <= "00";
+	Forward0_Branch <= "00";
+	Forward1_Branch <= "00";
+	Forward0_EX 	<= "00";
+	Forward1_EX 	<= "00";
 
-	if (EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rs)) then
-		Forward0_EX <= "01";
+	if (EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000")) then
+		if (MEM_Rd = EX_Rs) then
+			Forward0_EX <= "01";
+		end if;
+
+		if (Branch = '1') then
+			if (MEM_Rd = ID_Rs) then
+				Forward0_Branch <= "01";
+			end if;
+		end if;
 	end if;
 
-	if (EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rt)) then
-		Forward1_EX <= "01";
+	if (EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000")) then
+		if (MEM_Rd = EX_Rt) then
+			Forward1_EX <= "01";
+		end if;
+
+		if (Branch = '1') then
+			if (MEM_Rd = EX_Rt) then
+				Forward1_Branch <= "01";
+			end if;
+		end if;
 	end if;
 
-	if (MEM_WB_RegWrite = '1' and (WB_Rd /= "00000") and (not(EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rs))) and (WB_Rd = EX_Rs)) then
-		Forward0_EX <= "10";
+	if (MEM_WB_RegWrite = '1' and (WB_Rd /= "00000") and (not(EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rs)))) then
+		if (WB_Rd = EX_Rs) then
+			Forward0_EX <= "10";
+		end if;
+
+		if (Branch = '1') then
+			if (WB_Rd = ID_Rs) then
+				Forward0_Branch <= "10";
+			end if;
+		end if;
 	end if;
 
-	if (MEM_WB_RegWrite = '1' and (WB_Rd /= "00000") and (not(EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rt))) and (WB_Rd = EX_Rt)) then
-		Forward1_EX <= "10";
+	if (MEM_WB_RegWrite = '1' and (WB_Rd /= "00000") and (not(EX_MEM_RegWrite = '1' and (MEM_Rd /= "00000") and (MEM_Rd = EX_Rt)))) then
+		if (WB_Rd = EX_Rt) then
+			Forward1_EX <= "10";
+		end if;
+
+		if (Branch = '1') then
+			if (WB_Rd = ID_Rt) then
+				Forward1_Branch <= "10";
+			end if;
+		end if;
 	end if;
 
 	end process;
